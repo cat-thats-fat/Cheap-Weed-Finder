d9fd4194392b33d92e7fb690a0ed8df55dd1f3c1
cat-thats-fat <drbs2004@me.com>
2024-04-10 19:45:15 -0400

    Added menu and finished support for dutchie

---
+++ config.py
@@ -0,0 +1,15 @@
+#This is where you add dispensaries to be searched
+dutchIDS = {
+    "Giggles":  {
+        "id": "62e858e63967b40082a6aba1",
+        "discount": 10
+    },
+    "Avenue":  {
+        "id": "630693d206463800b24a8e4c",
+        "discount": 10
+    },
+#        "DISPENSARY NAME":  {
+#        "id": "VALUE",
+#        "discount": VALUE
+#    },
+}
