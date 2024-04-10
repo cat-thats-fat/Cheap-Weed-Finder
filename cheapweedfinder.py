4cfb58d168394b1bfb8b74862f33ed95a5e3d7a9
cat-thats-fat <drbs2004@me.com>
2024-04-10 17:11:13 -0400

    First verison of rewrite.
    Need to:
    
    Sort the final output by smallest dpg to biggest.
    
    add iheartjanesupport

--- cheapweedfinder.py
+++ cheapweedfinder.py
@@ -1,140 +1,48 @@
-#Title: CWFrequests
-#Purpose: To get the cheapest weed from the cloest four dispensaries
-#Version: 2.0
-#Date: 30/11/2023
-#To Do: 
-#   maybe add other dispensaries
-
-import os
 import json
 import requests
-from urllib.parse import parse_qs, unquote
+from urllib.parse import quote
 
-def janeRequest(data):
-    url = "https://vfm4x0n23a-1.algolianet.com/1/indexes/menu-products-production/query?x-algolia-agent=Algolia%20for%20JavaScript%20(4.14.2)%3B%20Browser"
+def dutchrequest(info , dutchfilters):
 
-    headers = {
-        "Accept": "*/*",
-        "Accept-Encoding": "gzip, deflate, br",
-        "Accept-Language": "en-US,en;q=0.9",
-        "Connection": "keep-alive",
-        "Content-Length": "166",
-        "Host": "vfm4x0n23a-2.algolianet.com",
-        "Origin": "https://www.iheartjane.com",
-        "Prefer": "safe",
-        "Referer": "https://www.iheartjane.com/",
-        "content-type": "application/x-www-form-urlencoded",
-        "sec-ch-ua": '"""Microsoft Edge";v="119", "Chromium";v="119", "Not?A_Brand";v="24"""',
-        "sec-ch-ua-mobile": "?0",
-        "sec-ch-ua-platform": "Windows",
-        "Sec-Fetch-Dest": "empty",
-        "Sec-Fetch-Mode": "cors",
-        "Sec-Fetch-Site": "cross-site",
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36",
-        "x-algolia-api-key": "b499e29eb7542dc373ec0254e007205d",
-        "x-algolia-application-id": "VFM4X0N23A",
-    }
+    #build the request
     
-    data = {
-        "query": "",
-        "filters": filters_str,
-        "userToken": "XssZ5RgH1tpicdzUVCsrH",
-        "hitsPerPage": 200,
+    url = "https://dutchie.com/graphql"
+    
+    variables = {
+        "includeEnterpriseSpecials": False,
+        "includeCannabinoids": True,
+        "productsFilter": {
+            "dispensaryId": info["id"],
+            "pricingType": "rec",
+            "strainTypes": [dutchfilters["strainType"][1]],
+            "subcategories": [],
+            "Status": "Active",
+            "types": [dutchfilters["category"][0]],
+            "useCache": False,
+            "sortDirection": 1,
+            "sortBy": "weight",
+            "isDefaultSort": True,
+            "bypassOnlineThresholds": False,
+            "isKioskMenu": False,
+            "removeProductsBelowOptionThresholds": True
+        },
+        "page": 0,
+        "perPage": 200
     }
 
-    r = requests.post(url, headers=headers, json=data)
-
-    json_data = json.loads(r.text)
-
-    json.dumps(json_data, indent=4, sort_keys=True)
-
-
-    mary_dir = {}
-    for hit in json_data['hits']:
-        mary_dir[hit['name']] = {
-            'category': hit['category'],
-            'brand': hit['brand'],
-            'discounted_price_eighth_ounce': hit['discounted_price_eighth_ounce'],
-            'discounted_price_gram': hit['discounted_price_gram'],
-            'discounted_price_half_gram': hit['discounted_price_half_gram'],
-            'discounted_price_half_ounce': hit['discounted_price_half_ounce'],
-            'discounted_price_ounce': hit['discounted_price_ounce'],
-            'discounted_price_quarter_ounce': hit['discounted_price_quarter_ounce'],
-            'discounted_price_two_gram': hit['discounted_price_two_gram'],
-            'price_eighth_ounce': hit['price_eighth_ounce'],
-            'price_gram': hit['price_gram'],
-            'price_half_gram': hit['price_half_gram'],
-            'price_half_ounce': hit['price_half_ounce'],
-            'price_ounce': hit['price_ounce'],
-            'price_quarter_ounce': hit['price_quarter_ounce'],
-            'price_two_gram': hit['price_two_gram'],
-            'dpg': [],
+    extensions = {
+        "persistedQuery": {
+            "version": 1,
+            "sha256Hash": "c75ea6b6d5a7bd30e52aed3b09da754c712817244e43a794c127da602ea06fce"
         }
+    }
 
-        removekeys = []
-        for key, value in mary_dir[hit['name']].items():
-            if "price" in key:
-                if value is None:
-                    removekeys.append(key)
-                    continue
-                if 'price_eighth_ounce' in key:
-                    dpg = (float(value) / 3.5)
-                    mary_dir[hit['name']]['dpg'].append([dpg, "3.5", value])
-                    removekeys.append(key)
-                    continue
-                elif 'price_quarter_ounce' in key:
-                    dpg = (float(value) / 7)
-                    mary_dir[hit['name']]['dpg'].append([dpg, "7", value])
-                    removekeys.append(key)
-                    continue
-                elif 'price_half_ounce' in key:
-                    dpg = (float(value) / 14)
-                    mary_dir[hit['name']]['dpg'].append([dpg, "14", value])
-                    removekeys.append(key)
-                    continue
-                elif 'price_ounce' in key:
-                    dpg = (float(value) / 28)
-                    mary_dir[hit['name']]['dpg'].append([dpg, "28", value])
-                    removekeys.append(key)
-                    continue
-                elif 'price_gram' in key:
-                    dpg = (float(value))
-                    mary_dir[hit['name']]['dpg'].append([dpg, "1", value])
-                    removekeys.append(key)
-                    continue
-                elif 'price_two_gram' in key:
-                    dpg = (float(value) / 2)
-                    mary_dir[hit['name']]['dpg'].append9([dpg , "2", value])
-                    removekeys.append(key)
-                    continue
-                elif 'price_half_gram' in key:
-                    dpg = (float(value) / 0.5)
-                    mary_dir[hit['name']]['dpg'].append([dpg, "3", value])
-                    removekeys.append(key)
-                    continue
-                else:
-                    print("Error at DPG calculation")
-                
-            else:
-                continue
-
-
-        mary_dir[hit['name']]['dpg'].sort()
-
-        for key in removekeys:
-            del mary_dir[hit['name']][key]
-
-    mary_dir = dict(sorted(mary_dir.items(), key=lambda item: item[1]['dpg'][0])[:5])
-
-    return mary_dir
-
-def dutchrequest(dutchID):
-    url = "https://dutchie.com/graphql"
     params = {
         "operationName": "FilteredProducts",
-        "variables": unquote(f"%7B%22includeEnterpriseSpecials%22%3Afalse%2C%22includeCannabinoids%22%3Atrue%2C%22productsFilter%22%3A%7B%22dispensaryId%22%3A%22{dutchID}%22%2C%22pricingType%22%3A%22rec%22%2C%22strainTypes%22%3A%5B%5D%2C%22subcategories%22%3A%5B%5D%2C%22Status%22%3A%22Active%22%2C%22types%22%3A%5B%22Flower%22%5D%2C%22useCache%22%3Afalse%2C%22sortDirection%22%3A1%2C%22sortBy%22%3A%22weight%22%2C%22isDefaultSort%22%3Atrue%2C%22bypassOnlineThresholds%22%3Afalse%2C%22isKioskMenu%22%3Afalse%2C%22removeProductsBelowOptionThresholds%22%3Atrue%7D%2C%22page%22%3A0%2C%22perPage%22%3A200%7D"),
-        "extensions": unquote("%7B%22persistedQuery%22%3A%7B%22version%22%3A1%2C%22sha256Hash%22%3A%22c75ea6b6d5a7bd30e52aed3b09da754c712817244e43a794c127da602ea06fce%22%7D%7D")
+        "variables": json.dumps(variables),
+        "extensions": json.dumps(extensions)
     }
+
     headers = {
         "sec-ch-ua": '"Microsoft Edge";v="119", "Chromium";v="119", "Not?A_Brand";v="24"',
         "apollographql-client-name": "Marketplace (production)",
@@ -143,72 +51,110 @@ def dutchrequest(dutchID):
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36 Edg/119.0.0.0",
         "content-type": "application/json",
         "accept": "*/*",
-        #"Referer": "https://dutchie.com/embedded-menu/710-kingston/products/flower?page=3&potencycbd=0%2C50&potencythc=0%2C50",
-        #"url": "https://dutchie.com/embedded-menu/710-kingston/products/flower?page=3&potencycbd=0%2C50&potencythc=0%2C50",
         "sec-ch-ua-platform": '"Windows"',
     }
 
-    response = requests.get(url, params=params, headers=headers)
+    # send the request
+    responseraw = requests.get(url, params=params, headers=headers)
 
-    json_data = json.loads(response.text)
+    response = json.loads(responseraw.text)
 
-    json.dumps(json_data, indent=4, sort_keys=True)
+    # extract the useful data from the response
 
-    dutch_dir = {}
-    for product in json_data["data"]["filteredProducts"]["products"]:
-        dutch_dir[product["Name"]] = {
-            "category": product["strainType"],
+    extracted = {}
+
+    for product in response["data"]["filteredProducts"]["products"]:
+        
+        # calculate dollars per gram
+        prices = product["Prices"]
+        weights = product["Options"]
+        discount = info["discount"]
+
+        options = []
+
+        for i in range(len(prices)):
+            currentoffer = []
+            weights[i] = weights[i].replace("g", "")
+            dpg = float(prices[i])/float(weights[i])
+            if discount != 0: currentoffer.append(dpg)
+            else:
+                finaldpg = dpg*(1-discount/100)
+                currentoffer.append(finaldpg)
+            currentoffer.extend([prices[i], weights[i]])
+            options.append(currentoffer)
+            
+        options.sort(key=lambda x: x[0])
+
+
+
+        extracted[product["Name"]] = {
             "brand": product["brandName"],
-            "weights": product["Options"],
-            "prices": product["recPrices"],
-            "specialPrices": product["recSpecialPrices"],
-            "dpg": []
+            "strainType": product["strainType"],
+            "options": options,
+            "thc": {
+                "unit": product["THCContent"]["unit"] if product["THCContent"] else "",
+                "range": product["THCContent"]["range"] if product["THCContent"] else "",
+            },
+            "cbd": {
+                "unit": product["CBDContent"]["unit"] if product["CBDContent"] else "",
+                "range": product["CBDContent"]["range"] if product["CBDContent"] else "",
+            },
         }
 
-        current = dutch_dir[product["Name"]]
+    # sort all offers by their best dpg(the first one bc its already sorted)
+    sorted = []
+    count = 0
 
-        if current["specialPrices"] is None:
-            del current["specialPrices"]
-            continue
-        elif current["specialPrices"] is not None:
-            for price in range(len(current["specialPrices"])):
-                current["prices"][price] = current["specialPrices"][price]
-            del current["specialPrices"]
+    #need to create a sorted list
+        
 
-        dpgtemp = []
-        for offer in range(len(current["prices"])):
-            current["weights"][offer] = current["weights"][offer].replace("g", "")
-            dpg = float(current["prices"][offer]) / float(current["weights"][offer])
-            dpgtemp.append([dpg, current["weights"][offer], current["prices"][offer]])
-            dpgtemp.sort(key=lambda x: x[0])
-        current["dpg"] = dpgtemp
-        del dpgtemp
+        
 
-        del current["prices"]
-        del current["weights"]
+    #Return the response
+
+    return sorted
+
+def maryrequest(maryID , category):
+    #Build the request
+    #send the request
+    #Format the response
+    #Return the response
+    return output
+
+#Where the store IDS are stored
+dutchIDS = {
+    "Giggles":  {
+        "id": "62e858e63967b40082a6aba1",
+        "discount": 10
+    },
+    "Avenue":  {
+        "id": "630693d206463800b24a8e4c",
+        "discount": 10
+    },
+}
+
+dutchfilters = {
+    "category": ["Flower", "Vaporizers", "Pre-Rolls", "Concentrate" ],
+    "strainType": ["Sativa", "Indica", "Hybrid"],
+    "discount": 10
+}
 
 
-    dutch_dir = dict(sorted(dutch_dir.items(), key=lambda item: item[1]['dpg'][0])[:5])
-
-    return dutch_dir
-
-
-dutchIDs = {"710": "5fefa138b2782100c5acd671", "Giggles": "62e858e63967b40082a6aba1"}
-janeIDs = {"MaryJ's" : 3217, "Inspired" : 4556}
 
 output = {}
 
-for key, value in janeIDs.items():
-    filters_str = f"store_id : {value} AND (root_types:\"flower\")"
-    output[key] = janeRequest(filters_str)
+if __name__ == "__main__":
+    #Loop through the IDs and request the data
+    loop = 0
+    if len(dutchIDS) > 0:
+        for name, info in dutchIDS.items():
+           output[name] = dutchrequest(info, dutchfilters)
+           loop += 1
+           print(loop)
+    
+    with open("output.json", "w") as f:
+        json.dump(output, f, indent=4)
+        f.close
 
-for key, value in dutchIDs.items():
-    output[key] = dutchrequest(value)
+    print("done")
 
-
-if os.path.exists('output.json'):
-    os.remove('output.json')
-
-with open('output.json', 'w') as f:
-   json.dump(output, f, indent=4)
-   f.close
